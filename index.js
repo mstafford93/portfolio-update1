c7534ccb1bd37000f5aec67a72f7b3470569c251
M Stafford <mgstafford@hotmail.co.uk>
2024-02-08 19:40:20 +0000

    First attemp responsiveness

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//In about section
 let startDate = new Date('09/05/2023');
 let dateNow = new Date();
 
@@ -6,3 +7,13 @@ let timeDiff = dateNow.getTime() - startDate.getTime();
 let dayDiff = Math.round(timeDiff/(1000*3600*24));
 
 document.getElementById('time-since').innerHTML = 'I started coding around '+ startDate.toDateString() + ' and the date you are viewing this is ' + dateNow.toDateString() + ' so I have been learning to code for ' + dayDiff + ' days!';
+
+//menu-icon
+
+let menuIcon = document.querySelector("#menu-icon")
+let navbar = document.querySelector(".navbar")
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle("fa-xmark");
+    navbar.classList.toggle('active')
+}
